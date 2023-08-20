445aaf78f5408e5638f61be8d174dcd9e48e1fc4
naveenvenk17 <naveenvenkat1711@gmail.com>
2023-08-20 17:04:59 +0530

    added data ingestion

--- src/MLopsProject/utils/common.py
+++ src/MLopsProject/utils/common.py
@@ -1,60 +1,38 @@
 import os
-import box
-from box import BoxValueError
+from box.exceptions import BoxValueError
 import yaml
 from MLopsProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
-from box import config_box
+from box import ConfigBox
 from pathlib import Path
 from typing import Any
-from box import ConfigBox
 
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """
-    reads YAML file and returns 
+    """reads yaml file and returns
 
     Args:
-        path_to_yaml (str) : path like input
+        path_to_yaml (str): path like input
 
     Raises:
-        ValueError : if yaml file is empty
+        ValueError: if yaml file is empty
         e: empty file
 
     Returns:
-        ConfigBox: ConfigBox type 
+        ConfigBox: ConfigBox type
     """
-
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml_file: {path_to_yaml} loaded successfully")
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
-
     except BoxValueError:
-        raise ValueError('yaml file is empty')
-
+        raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
-
-
-@ensure_annotations
-def create_directories(path_to_directories: list, verbose=True):
-    """
-    create a list of directories
-
-    Args:
-       path_to_directories (list) : list of path of directories
-       ignore_log (bool , optional) ignore if multiple dirs are planning to be created. Default set to false.
-    """
-
-    for path in path_to_directories:
-        os.makedirs(path, exist_ok=True)
-        if verbose:
-            logger.info(f"created directory at : {path}")
 
 
 @ensure_annotations
